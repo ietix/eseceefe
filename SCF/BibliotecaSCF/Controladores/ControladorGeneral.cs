1d831082223375cc77289a0b88df1b2a4f23f714
ietix <comikfreak@gmail.com>
2017-05-05 00:08:40 -0300

    - Columns puntoDeVenta.Numero and tipoComprobante.Descripcion to reporteFacturas.rdlc

--- SCF/BibliotecaSCF/Controladores/ControladorGeneral.cs
+++ SCF/BibliotecaSCF/Controladores/ControladorGeneral.cs
@@ -2777,56 +2777,57 @@ namespace BibliotecaSCF.Controladores
           }
       }
 
-      public static DataTable RecuperarFacturasPorFechas(DateTime fechaHoraDesde, DateTime fechaHoraHasta)
+    public static DataTable RecuperarFacturasPorFechas(DateTime fechaHoraDesde, DateTime fechaHoraHasta)
+    {
+      var nhSesion = ManejoDeNHibernate.IniciarSesion();
+
+      try
       {
-          ISession nhSesion = ManejoDeNHibernate.IniciarSesion();
+        var tablaFacturas = new DataTable();
+        tablaFacturas.Columns.Add("codigoFactura");
+        tablaFacturas.Columns.Add("numeroFactura");
+        tablaFacturas.Columns.Add("fechaFacturacion");
+        tablaFacturas.Columns.Add("descripcionTipoComprobante");
+        tablaFacturas.Columns.Add("descripcionTipoMoneda");
+        tablaFacturas.Columns.Add("descripcionConcepto");
+        tablaFacturas.Columns.Add("descripcionIVA");
+        tablaFacturas.Columns.Add("subtotal");
+        tablaFacturas.Columns.Add("total");
+        tablaFacturas.Columns.Add("cae");
+        tablaFacturas.Columns.Add("fechaVencimientoCAE");
+        tablaFacturas.Columns.Add("remitos");
+        tablaFacturas.Columns.Add("condicionVenta");
+        tablaFacturas.Columns.Add("codigoDireccion");
+        tablaFacturas.Columns.Add("direccion");
+        tablaFacturas.Columns.Add("domicilio");
+        tablaFacturas.Columns.Add("localidad");
+        tablaFacturas.Columns.Add("cotizacion");
+        tablaFacturas.Columns.Add("cliente");
+        tablaFacturas.Columns.Add("numeroPuntoDeVenta");
 
-          try
-          {
-              DataTable tablaFacturas = new DataTable();
-              tablaFacturas.Columns.Add("codigoFactura");
-              tablaFacturas.Columns.Add("numeroFactura");
-              tablaFacturas.Columns.Add("fechaFacturacion");
-              tablaFacturas.Columns.Add("descripcionTipoComprobante");
-              tablaFacturas.Columns.Add("descripcionTipoMoneda");
-              tablaFacturas.Columns.Add("descripcionConcepto");
-              tablaFacturas.Columns.Add("descripcionIVA");
-              tablaFacturas.Columns.Add("subtotal");
-              tablaFacturas.Columns.Add("total");
-              tablaFacturas.Columns.Add("cae");
-              tablaFacturas.Columns.Add("fechaVencimientoCAE");
-              tablaFacturas.Columns.Add("remitos");
-              tablaFacturas.Columns.Add("condicionVenta");
-              tablaFacturas.Columns.Add("codigoDireccion");
-              tablaFacturas.Columns.Add("direccion");
-              tablaFacturas.Columns.Add("domicilio");
-              tablaFacturas.Columns.Add("localidad");
-              tablaFacturas.Columns.Add("cotizacion");
-              tablaFacturas.Columns.Add("cliente");
+        var listaFacturas = CatalogoFactura.RecuperarLista(x => x.FechaFacturacion >= fechaHoraDesde && x.FechaFacturacion <= fechaHoraHasta, nhSesion);
 
-              List<Factura> listaFacturas = CatalogoFactura.RecuperarLista(x => x.FechaFacturacion >= fechaHoraDesde && x.FechaFacturacion <= fechaHoraHasta, nhSesion);
+        listaFacturas.OrderBy(x => x.NumeroFactura).Aggregate(tablaFacturas, (dt, r) =>
+        {
+          dt.Rows.Add(r.Codigo, r.NumeroFactura, r.FechaFacturacion.ToString("dd/MM/yyyy"), r.TipoComprobante.Descripcion, r.Moneda.Descripcion,
+              r.Concepto.Descripcion, r.Iva.Descripcion, r.Subtotal, r.Total, r.Cae, r.FechaVencimiento, string.Join(", ", r.Entregas.Select(x => x.NumeroRemito)),
+              r.CondicionVenta, r.Entregas[0].Direccion.Codigo, r.Entregas[0].Direccion.Descripcion + ", " + r.Entregas[0].Direccion.Localidad + ", " +
+              r.Entregas[0].Direccion.Provincia, r.Entregas[0].Direccion.Descripcion, r.Entregas[0].Direccion.Localidad, r.Cotizacion,
+              r.Entregas[0].NotaDePedido.Cliente.NumeroDocumento + " - " + r.Entregas[0].NotaDePedido.Cliente.RazonSocial, r.PuntoDeVenta.Numero); return dt;
+        });
 
-              listaFacturas.OrderBy(x => x.NumeroFactura).Aggregate(tablaFacturas, (dt, r) =>
-              {
-                  dt.Rows.Add(r.Codigo, r.NumeroFactura, r.FechaFacturacion.ToString("dd/MM/yyyy"), r.TipoComprobante.Descripcion, r.Moneda.Descripcion,
-                      r.Concepto.Descripcion, r.Iva.Descripcion, r.Subtotal, r.Total, r.Cae, r.FechaVencimiento, string.Join(", ", r.Entregas.Select(x => x.NumeroRemito)),
-                      r.CondicionVenta, r.Entregas[0].Direccion.Codigo, r.Entregas[0].Direccion.Descripcion + ", " + r.Entregas[0].Direccion.Localidad + ", " +
-                      r.Entregas[0].Direccion.Provincia, r.Entregas[0].Direccion.Descripcion, r.Entregas[0].Direccion.Localidad, r.Cotizacion,
-                      r.Entregas[0].NotaDePedido.Cliente.NumeroDocumento + " - " + r.Entregas[0].NotaDePedido.Cliente.RazonSocial); return dt;
-              });
-
-              return tablaFacturas;
-          }
-          catch (Exception ex)
-          {
-              throw ex;
-          }
-          finally
-          {
-              nhSesion.Close();
-              nhSesion.Dispose();
-          }
+        return tablaFacturas;
       }
+      catch (Exception ex)
+      {
+        throw ex;
+      }
+      finally
+      {
+        nhSesion.Close();
+        nhSesion.Dispose();
+      }
+    }
 
     public static DataTable RecuperarFacturaPorPuntoDeVenta(int codigoPuntoDeVenta)
     {
--- SCF/SCF/dashboard/reporte_facturas.aspx.cs
+++ SCF/SCF/dashboard/reporte_facturas.aspx.cs
@@ -10,67 +10,61 @@ using Microsoft.Reporting.WebForms;
 
 namespace SCF.dashboard
 {
-    public partial class reporte_facturas : System.Web.UI.Page
+  public partial class reporte_facturas : System.Web.UI.Page
+  {
+    dtReporteFacturas dtReporte = new dtReporteFacturas();
+    DataTable tablaReporte = new DataTable();
+
+    protected void Page_Load(object sender, EventArgs e)
     {
-        dtReporteFacturas dtReporte = new dtReporteFacturas();
-        DataTable tablaReporte = new DataTable();
-
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            //EmitirReporte();
-        }
-
-        protected void btnGenerarReporte_Click(object sender, EventArgs e)
-        {
-            EmitirReporte();
-        }
-
-        private void EmitirReporte()
-        {
-            DataTable dtRecuperarFacturas = ControladorGeneral.RecuperarFacturasPorFechas(DateTime.Parse(deFechaDesde.Value.ToString()), DateTime.Parse(deFechaHasta.Value.ToString()));
-            
-            rvFacturas.ProcessingMode = ProcessingMode.Local;
-            rvFacturas.LocalReport.EnableExternalImages = true;
-            rvFacturas.LocalReport.ReportPath = Server.MapPath("..") + "\\reportes\\reporteFacturas.rdlc";
-
-            ReportParameter txtFechaDesde = new ReportParameter("txtFechaDesde", deFechaDesde.Value.ToString());
-            ReportParameter txtFechaHasta = new ReportParameter("txtFechaHasta", deFechaHasta.Value.ToString());
-
-            this.rvFacturas.LocalReport.SetParameters(new ReportParameter[] { txtFechaDesde,txtFechaHasta});
-
-            dtReporte.DataTable1.Clear();
-            tablaReporte = dtRecuperarFacturas;
-
-            foreach (DataRow fila in tablaReporte.Rows)
-            {
-                double iva = double.Parse(fila["total"].ToString()) - double.Parse(fila["subtotal"].ToString());
-                DataRow filaReporte = dtReporte.DataTable1.NewRow();
-                filaReporte["numeroFactura"] = fila["numeroFactura"];
-                filaReporte["fechaEmision"] = fila["fechaFacturacion"];
-                filaReporte["nombreCliente"] = fila["cliente"];
-                filaReporte["tipoMoneda"] = fila["descripcionTipoMoneda"];
-                filaReporte["cotizacion"] = fila["cotizacion"];
-                filaReporte["subtotal"] = fila["subtotal"];
-                filaReporte["iva"] = iva;
-                filaReporte["total"] = fila["total"];
-
-                dtReporte.DataTable1.Rows.Add(filaReporte);
-            }
-
-            //for (int i = 0; i < (5 - tablaReporte.Rows.Count); i++)
-            //for (int i = 0; i < (tablaReporte.Rows.Count); i++)
-            /*for (int i = tablaReporte.Rows.Count; i <= 10; i++)
-            {
-                DataRow filaReporte = dtReporteFacturas.DataTable1.NewRow();
-                dtReporteFacturas.DataTable1.Rows.Add(filaReporte);
-            }*/
-
-            dtReporteFacturas dsReporte1 = dtReporte;
-
-            ReportDataSource datasource = new ReportDataSource("DataSet1", dsReporte1.Tables[0]);
-            rvFacturas.LocalReport.DataSources.Clear();
-            rvFacturas.LocalReport.DataSources.Add(datasource);
-            rvFacturas.LocalReport.Refresh();
-        }
+      
     }
+
+    protected void btnGenerarReporte_Click(object sender, EventArgs e)
+    {
+        EmitirReporte();
+    }
+
+    private void EmitirReporte()
+    {
+      var dtRecuperarFacturas = ControladorGeneral.RecuperarFacturasPorFechas(DateTime.Parse(deFechaDesde.Value.ToString()), DateTime.Parse(deFechaHasta.Value.ToString()));
+            
+      rvFacturas.ProcessingMode = ProcessingMode.Local;
+      rvFacturas.LocalReport.EnableExternalImages = true;
+      rvFacturas.LocalReport.ReportPath = Server.MapPath("..") + "\\reportes\\reporteFacturas.rdlc";
+
+      var txtFechaDesde = new ReportParameter("txtFechaDesde", deFechaDesde.Value.ToString());
+      var txtFechaHasta = new ReportParameter("txtFechaHasta", deFechaHasta.Value.ToString());
+
+      this.rvFacturas.LocalReport.SetParameters(new ReportParameter[] { txtFechaDesde,txtFechaHasta});
+
+      dtReporte.DataTable1.Clear();
+      tablaReporte = dtRecuperarFacturas;
+
+      foreach (DataRow fila in tablaReporte.Rows)
+      {
+        var iva = double.Parse(fila["total"].ToString()) - double.Parse(fila["subtotal"].ToString());
+        var filaReporte = dtReporte.DataTable1.NewRow();
+        filaReporte["tipoComprobante"] = fila["descripcionTipoComprobante"];
+        filaReporte["puntoDeVenta"] = fila["numeroPuntoDeVenta"] == DBNull.Value ? string.Empty : Convert.ToInt32(fila["numeroPuntoDeVenta"]).ToString("D4");
+        filaReporte["numeroFactura"] = Convert.ToInt32(fila["numeroFactura"]).ToString("D8");
+        filaReporte["fechaEmision"] = fila["fechaFacturacion"];
+        filaReporte["nombreCliente"] = fila["cliente"];
+        filaReporte["tipoMoneda"] = fila["descripcionTipoMoneda"];
+        filaReporte["cotizacion"] = fila["cotizacion"];
+        filaReporte["subtotal"] = fila["subtotal"];
+        filaReporte["iva"] = iva;
+        filaReporte["total"] = fila["total"];
+
+        dtReporte.DataTable1.Rows.Add(filaReporte);
+      }
+
+      dtReporteFacturas dsReporte1 = dtReporte;
+
+      var datasource = new ReportDataSource("DataSet1", dsReporte1.Tables[0]);
+      rvFacturas.LocalReport.DataSources.Clear();
+      rvFacturas.LocalReport.DataSources.Add(datasource);
+      rvFacturas.LocalReport.Refresh();
+    }
+  }
 }
